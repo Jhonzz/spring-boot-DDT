eafa2febe4d253f19381d547ef8c111d34dabd59
jhonzz <jhonpablo756@gmail.com>
2025-11-06 12:25:17 -0300

    adding context path and get hi

--- anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -3,10 +3,10 @@ package academy.devdojo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("hi")
     public String hi(){
         return "OMAE WA MOU SHINDERE IRU";
     }

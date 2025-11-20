7911ac454cf60c459539a2737354bd053ae024db
jhonzz <jhonpablo756@gmail.com>
2025-11-20 10:40:27 -0300

    13 and exercice 03

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("v1/animes")
@@ -12,19 +13,27 @@ import java.util.List;
 public class AnimeController {
 
     @GetMapping()
-    public List<Anime> listAllAnimes(@RequestParam(required = false) String animeName){
+    public List<Anime> listAllAnimes(@RequestParam(required = false) String animeName) {
         if (animeName == null) {
-            return Anime.animeList();
+            return Anime.getAnimes();
         }
-        return Anime.animeList().stream().filter(anime -> animeName.equalsIgnoreCase(anime.getName())).toList();
+        return Anime.getAnimes().stream().filter(anime -> animeName.equalsIgnoreCase(anime.getName())).toList();
     }
+
     @GetMapping("{id}")
-    public Anime findById(@PathVariable Long id){
+    public Anime findById(@PathVariable Long id) {
         return Anime.
-                animeList().
+                getAnimes().
                 stream().
                 filter(anime -> id.equals(anime.getId())).
                 findFirst().
                 orElse(null);
+    }
+
+    @PostMapping()
+    public Anime saveAnime(@RequestBody Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(1, 1000));
+        Anime.getAnimes().add(anime);
+        return anime;
     }
 }
--- anime-service/src/main/java/academy/devdojo/domain/Anime.java
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -1,22 +1,28 @@
 package academy.devdojo.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class Anime {
     private long id;
     private String name;
 
-    public static List<Anime> animeList() {
+    @Getter
+    private static List<Anime> animes = new ArrayList<>();
+
+    static {
         Anime berserk = new Anime(1L, "Berserk");
         Anime dandadan = new Anime(2L, "Dandadan");
         Anime dragonBall = new Anime(3L, "Dragon ball");
         Anime naruto = new Anime(4L, "Naruto");
-
-        return List.of(berserk, dandadan, dragonBall, naruto);
+        animes.addAll(List.of(berserk, dandadan, dragonBall, naruto));
     }
 }

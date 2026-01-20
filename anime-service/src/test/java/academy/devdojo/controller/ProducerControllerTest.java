bb9e4e6c97f014418f2857e3829869b2c1141610
jhon <jhonpablo756@gmail.com>
2026-01-20 20:33:18 -0300

    Adding file response to unit tests and componentScan to Test class

--- anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -13,34 +13,48 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebMvcTest(controllers = ProducerController.class) //Não carrega as classes @Component, então voce precisa adiciona-las no @import
+@WebMvcTest(controllers = ProducerController.class)
+//Não carrega as classes @Component, então voce precisa adiciona-las no @import
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Import({ProducerMapperImpl.class, ProducerService.class, ProducerService.class, ProducerHardCodedRepository.class, ProducerData.class})
+@ComponentScan(basePackages = "academy.devdojo") // will scan all the beans in the package
+//@Import({ProducerMapperImpl.class, ProducerService.class, ProducerService.class, ProducerHardCodedRepository.class, ProducerData.class}) can be used in a application with excessive beans, so the process would cost less
 class ProducerControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockitoBean
     private ProducerData producerData;
     private List<Producer> producerList;
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @BeforeEach
-    void init(){
-        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
-        var madHouse = Producer.builder().id(2L).name("MadHouse").createdAt(LocalDateTime.now()).build();
-        var saru = Producer.builder().id(3L).name("Saru").createdAt(LocalDateTime.now()).build();
-        var studioGhibli = Producer.builder().id(4L).name("StudioGhibli").createdAt(LocalDateTime.now()).build();
-        producerList = new ArrayList<>(List.of(ufotable, madHouse,saru, studioGhibli));
+    void init() {
+        var dateTime = "2026-01-20T20:09:29.4158618";
+        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS");
+        var localDateTime = LocalDateTime.parse(dateTime, formatter);
+
+        var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(localDateTime).build();
+        var madHouse = Producer.builder().id(2L).name("MadHouse").createdAt(localDateTime).build();
+        var saru = Producer.builder().id(3L).name("Saru").createdAt(localDateTime).build();
+        var studioGhibli = Producer.builder().id(4L).name("StudioGhibli").createdAt(localDateTime).build();
+        producerList = new ArrayList<>(List.of(ufotable, madHouse, saru, studioGhibli));
     }
 
     @Test
@@ -48,10 +62,16 @@ class ProducerControllerTest {
     @Order(1)
     void findAll_ReturnsAllProducers_WhenParameterIsNull() throws Exception {
         BDDMockito.when(producerData.getProducers()).thenReturn(producerList);
+        var response = readResourceFile("producer/get-producer-null-name-200.json");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    private String readResourceFile(String fileName) throws IOException {
+        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));
     }
 }

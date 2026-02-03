4fd9b94d41b6def1fb2e4d169238ddcaec7a9176
Jhonzz <jhonpablo756@gmail.com>
2026-02-03 00:31:07 -0300

    profile pt 01

--- anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
+++ anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
@@ -1,11 +1,19 @@
 package academy.devdojo.config;
 
 import externalDependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectionConfiguration {
+    @Value("${database.url") //will get from the yml for example database.mongo.url, using defined in run config
+    private String url;
+    @Value("${database.username")
+    private String username;
+    @Value("${database.password")
+    private String password;
+
     @Bean
     public Connection connectionMySql(){
         return new Connection("localhost", "devdojoMySql","vegeta");
--- anime-service/src/main/java/externalDependency/Connection.java
+++ anime-service/src/main/java/externalDependency/Connection.java
@@ -1,10 +1,13 @@
 package externalDependency;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @AllArgsConstructor
+@Getter
 public class Connection {
     private String host;
     private String username;

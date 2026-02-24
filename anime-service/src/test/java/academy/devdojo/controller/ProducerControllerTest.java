0bc8a64d1ab9e71ec4f495f2cfda3e6c98abeb0b
Jhonzz <jhonpablo756@gmail.com>
2026-02-24 12:54:35 -0300

    fixing dependencies and imports of mvctest  in anime-service

--- anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ anime-service/src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.*;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;

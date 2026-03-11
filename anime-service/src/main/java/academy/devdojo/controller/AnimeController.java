886ddb5ed8c04f80a7185d25d91c34a7561ae35b
Jhonzz <jhonpablo756@gmail.com>
2026-03-11 08:55:43 -0300

    Adding Commons Core

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -13,7 +13,6 @@ import academy.devdojo.service.AnimeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;

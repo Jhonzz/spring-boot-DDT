c043e7d817218f8f734b357872310bcd7e561828
Jhonzz <jhonpablo756@gmail.com>
2026-04-14 09:38:33 -0300

    Adding many to one between user and profile

---
+++ user-service/src/main/java/academy/devdojo/domain/UserProfile.java
@@ -0,0 +1,23 @@
+package academy.devdojo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@With
+@Data
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "devdojo_user")
+public class UserProfile {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false)
+    private User user;
+    @ManyToOne(optional = false)
+    private Profile profile;
+}

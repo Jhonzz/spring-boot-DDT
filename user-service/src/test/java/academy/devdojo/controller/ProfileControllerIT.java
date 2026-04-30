970a92cf8f822593588683eaad9fe92a10203d3a
Jhonzz <jhonpablo756@gmail.com>
2026-04-30 15:37:29 -0300

    Adding rainy cenario to profile integration tests - java crumbs

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
@@ -3,9 +3,12 @@ package academy.devdojo.controller;
 import academy.devdojo.DTO.response.ProfileGetResponse;
 import academy.devdojo.DTO.response.ProfilePostResponse;
 import academy.devdojo.commons.FileUtils;
-import jakarta.annotation.Nonnull;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -14,6 +17,7 @@ import org.springframework.http.*;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -76,10 +80,42 @@ class ProfileControllerIT {
                 .hasNoNullFieldsOrProperties();
     }
 
+    @ParameterizedTest
+    @MethodSource("postProfileBadRequestSource")
+    @DisplayName("POST v1/profiles returns bad request when fields are invalid")
+    @Order(4)
+    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String requestFile, String responseFile) throws Exception {
+        var request = fileUtils.readResourceFile("profile/%s".formatted(requestFile));
+        var expectedResponse = fileUtils.readResourceFile("profile/%s".formatted(responseFile));
+        var profileEntityRequest = buildHttpEntity(request);
+
+        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.POST, profileEntityRequest, String.class);
+
+        Assertions
+                .assertThat(responseEntity.getStatusCode())
+                .isEqualTo(HttpStatus.BAD_REQUEST);
+
+        Assertions
+                .assertThat(responseEntity)
+                .isNotNull();
+
+        JsonAssertions.assertThatJson(responseEntity.getBody())
+                .whenIgnoringPaths("timestamp") //ignore timestamp (because it will always change)
+                .isEqualTo(expectedResponse);
+    }
+
+    private static Stream<Arguments> postProfileBadRequestSource() {
+        return Stream.of(
+                Arguments.of("post-request-profile-empty-fields-400.json", "post-response-profile-empty-fields-400.json"),
+                Arguments.of("post-request-profile-blank-fields-400.json", "post-response-profile-blank-fields-400.json")
+        );
+    }
+
     private static HttpEntity<String> buildHttpEntity(String request) {
         var httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
         return new HttpEntity<>(request, httpHeaders);
     }
+
 }

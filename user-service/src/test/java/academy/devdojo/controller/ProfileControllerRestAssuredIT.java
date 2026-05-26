695ea3b65c19e4666793f70a6eb6c10fbcde05be
Jhonzz <jhonpablo756@gmail.com>
2026-05-26 09:43:17 -0300

    Adding rest assured tests to profile exceptions

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
@@ -7,11 +7,19 @@ import io.restassured.http.ContentType;
 import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -24,7 +32,7 @@ class ProfileControllerRestAssuredIT extends IntegrationTestConfig {
 
 
     @BeforeEach
-    void setUrl(){
+    void setUrl() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
     }
@@ -87,42 +95,34 @@ class ProfileControllerRestAssuredIT extends IntegrationTestConfig {
                 .whenIgnoringPaths("id")
                 .isEqualTo(expectedResponse);
     }
-//
-//    @ParameterizedTest
-//    @MethodSource("postProfileBadRequestSource")
-//    @DisplayName("POST v1/profiles returns bad request when fields are invalid")
-//    @Order(4)
-//    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String requestFile, String responseFile) throws Exception {
-//        var request = fileUtils.readResourceFile("profile/%s".formatted(requestFile));
-//        var expectedResponse = fileUtils.readResourceFile("profile/%s".formatted(responseFile));
-//        var profileEntityRequest = buildHttpEntity(request);
-//
-//        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.POST, profileEntityRequest, String.class);
-//
-//        Assertions
-//                .assertThat(responseEntity.getStatusCode())
-//                .isEqualTo(HttpStatus.BAD_REQUEST);
-//
-//        Assertions
-//                .assertThat(responseEntity)
-//                .isNotNull();
-//
-//        JsonAssertions.assertThatJson(responseEntity.getBody())
-//                .whenIgnoringPaths("timestamp") //ignore timestamp (because it will always change)
-//                .isEqualTo(expectedResponse);
-//    }
-//
-//    private static Stream<Arguments> postProfileBadRequestSource() {
-//        return Stream.of(
-//                Arguments.of("post-request-profile-empty-fields-400.json", "post-response-profile-empty-fields-400.json"),
-//                Arguments.of("post-request-profile-blank-fields-400.json", "post-response-profile-blank-fields-400.json")
-//        );
-//    }
-//
-//    private static HttpEntity<String> buildHttpEntity(String request) {
-//        var httpHeaders = new HttpHeaders();
-//        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-//
-//        return new HttpEntity<>(request, httpHeaders);
-//    }
+
+    @ParameterizedTest
+    @MethodSource("postProfileBadRequestSource")
+    @DisplayName("POST v1/profiles returns bad request when fields are invalid")
+    @Order(4)
+    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String requestFile, String responseFile) {
+        var request = fileUtils.readResourceFile("profile/%s".formatted(requestFile));
+        var expectedResponse = fileUtils.readResourceFile("profile/%s".formatted(responseFile));
+
+        String response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("timestamp")
+                .isEqualTo(expectedResponse);
+    }
+
+    private static Stream<Arguments> postProfileBadRequestSource() {
+        return Stream.of(
+                Arguments.of("post-request-profile-empty-fields-400.json", "post-response-profile-empty-fields-400.json"),
+                Arguments.of("post-request-profile-blank-fields-400.json", "post-response-profile-blank-fields-400.json")
+        );
+    }
 }

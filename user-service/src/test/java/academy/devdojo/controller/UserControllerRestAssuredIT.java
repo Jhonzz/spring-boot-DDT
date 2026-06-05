90810ad6c76b6630345555766f3c59232bba19b8
Jhonzz <jhonpablo756@gmail.com>
2026-06-05 15:54:16 -0300

    Adding integration tests with rest assured

--- user-service/src/test/java/academy/devdojo/controller/UserControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/UserControllerRestAssuredIT.java
@@ -2,9 +2,12 @@ package academy.devdojo.controller;
 
 import academy.devdojo.commons.FileUtils;
 import academy.devdojo.config.IntegrationTestConfig;
+import academy.devdojo.repository.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import net.javacrumbs.jsonunit.assertj.JsonAssertions;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +23,8 @@ class UserControllerRestAssuredIT extends IntegrationTestConfig {
     private FileUtils fileUtils;
     @LocalServerPort
     private int port;
+    @Autowired
+    private UserRepository userRepository;
 
     @BeforeEach
     void setUrl() {
@@ -53,6 +58,78 @@ class UserControllerRestAssuredIT extends IntegrationTestConfig {
 
         JsonAssertions.assertThatJson(response)
                 .whenIgnoringPaths("[*].id")
+                .isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("GET v1/users?firstName=Ging returns a list with the found user")
+    @Sql(value = "/sql/user/init_one_user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/user/clean_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Order(2)
+    void findByName_ReturnsUserFoundInList_WhenUserIsFound() {
+        var expectedResponse = fileUtils.readResourceFile("user/get-user-ging-first-name-200.json");
+
+        var firstName = "Ging";
+
+        var response = RestAssured.given()
+                .when()
+                .queryParam("firstName", firstName)
+                .get(URL).
+                then()
+                .statusCode(HttpStatus.OK.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .and(user -> {
+                    user.node("[0].id").asNumber().isPositive();
+                });
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("[*].id")
+                .isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("GET v1/users?firstName=x returns a empty list")
+    @Order(3)
+    void findByName_ReturnsEmptyList_WhenUserIsNotFound() {
+        var expectedResponse = fileUtils.readResourceFile("user/get-user-x-first-name-200.json");
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .when()
+                .queryParam("firstName", "Gon")
+                .get(URL)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(Matchers.equalTo(expectedResponse))
+                .log().all();
+    }
+
+    @Test
+    @DisplayName("GET v1/users/1 returns an user with given id")
+    @Sql(value = "/sql/user/init_one_user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/user/clean_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Order(4)
+    void findById_ReturnsUserById_WhenSuccessful(){
+        var expectedResponse = fileUtils.readResourceFile("user/get-user-by-id-200.json");
+        var users = userRepository.findByFirstNameIgnoreCase("Ging");
+
+        Assertions.assertThat(users).hasSize(1);
+
+        RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .pathParam("id", users.getFirst().getId())
+                .get(URL + "/{id}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(expectedResponse)
+                .whenIgnoringPaths("id")
                 .isEqualTo(expectedResponse);
     }
 }

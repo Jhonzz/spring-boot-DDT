7b46835cfe321f7c6a2676e445d4651141f2ca05
Jhonzz <jhonpablo756@gmail.com>
2026-06-25 20:26:44 -0300

    fixing user's integration tests

--- commons-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
+++ commons-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
@@ -11,33 +11,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalErrorHandlerAdvice {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<DefaultErrorMessage> handleNotFoundException(NotFoundException e){
+    public ResponseEntity<DefaultErrorMessage> handleNotFoundException(NotFoundException e) {
         var error = new DefaultErrorMessage(HttpStatus.NOT_FOUND.value(), e.getReason());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<DefaultErrorMessage> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e){
+    public ResponseEntity<DefaultErrorMessage> handleSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException e) {
         var error = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), "Duplicated entry for one of the unique fields");
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request){
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
 
         var defaultMessage = e.getBindingResult()
                 .getAllErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.joining(", "));
+                .filter(Objects::nonNull) // to not get NullPointerException
+                .sorted()
+                .collect(Collectors.joining(", ")); //each defaultMessage will put ", " after the message
 
         var apiError = ApiError.builder()
                 .timestamp(OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME))

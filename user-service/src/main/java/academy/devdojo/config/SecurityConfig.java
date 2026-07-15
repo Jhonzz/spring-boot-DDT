fa00fb1189d1e24b128db09b8d2970fe2ee02d4e
Jhonzz <jhonpablo756@gmail.com>
2026-07-14 23:19:12 -0300

    Adding authorization to requests

--- user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
+++ user-service/src/main/java/academy/devdojo/config/SecurityConfig.java
@@ -3,37 +3,48 @@ package academy.devdojo.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @Log4j2
 public class SecurityConfig {
+    private static final String[] WHITE_LIST = {"/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**"};
 
-     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
-         log.info(passwordEncoder.encode("ippo"));
-         var user = User.withUsername("takamura")
-                 .password(passwordEncoder.encode("ippo"))
-                 .roles("USER")
-                 .build();
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        log.info(passwordEncoder.encode("ippo"));
+        var user = User.withUsername("takamura")
+                .password(passwordEncoder.encode("ippo"))
+                .roles("USER")
+                .build();
 
-         var admin = User.withUsername("admin")
-                 .password(passwordEncoder.encode("devdojo"))
-                 .roles("ADMIN")
-                 .build();
+        var admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("devdojo"))
+                .roles("ADMIN")
+                .build();
 
-         return new InMemoryUserDetailsManager(user, admin);
-     }
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
-     @Bean
-    public PasswordEncoder passwordEncoder(){
-         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.requestMatchers(WHITE_LIST).permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()) //popup in web requests to login
+                .build();
+    }
 
 }

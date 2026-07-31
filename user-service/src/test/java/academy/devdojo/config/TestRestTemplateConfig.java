4f289e0875ae31ec03ae35cd9b5b9a7347b65a92
Jhonzz <jhonpablo756@gmail.com>
2026-07-31 10:12:02 -0300

    Adding auth config to RestAssured tests

--- user-service/src/test/java/academy/devdojo/config/TestRestTemplateConfig.java
+++ user-service/src/test/java/academy/devdojo/config/TestRestTemplateConfig.java
@@ -1,13 +1,15 @@
 package academy.devdojo.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
-@Configuration
+import static academy.devdojo.commons.Constants.*;
+
+@TestConfiguration
 @Lazy //will run just after to not return error when it doesn't found the container(tomcat)
 public class TestRestTemplateConfig {
     @LocalServerPort
@@ -15,9 +17,9 @@ public class TestRestTemplateConfig {
 
     @Bean
     public TestRestTemplate testRestTemplate() {
-        var uri = new DefaultUriBuilderFactory("http://localhost:" + port);
+        var uri = new DefaultUriBuilderFactory(BASE_URI + port);
         var testRestTemplate = new TestRestTemplate()
-                .withBasicAuth("mestre.kame@DBZ.com", "test");
+                .withBasicAuth(REGULAR_USERNAME, PASSWORD);
         testRestTemplate.setUriTemplateHandler(uri);
 
         return testRestTemplate;

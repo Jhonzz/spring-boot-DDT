3ad4968a253cc90cb60a9593a531f73fa3f3a010
Jhonzz <jhonpablo756@gmail.com>
2026-08-24 16:10:16 -0300

    Adding Exception handler to invalid cep integration

---
+++ user-service/src/main/java/academy/devdojo/dto/response/CepErrorResponse.java
@@ -0,0 +1,8 @@
+package academy.devdojo.dto.response;
+
+import java.util.List;
+
+public record CepErrorResponse(String name, String message, String type, List<ErrorResponse> errors) {}
+
+record ErrorResponse(String name, String message, String service) {}
+
